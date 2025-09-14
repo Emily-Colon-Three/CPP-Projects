63a15023b8f7be2873f721414eedbccfae7b865c
Emily <Emily.Olsen.Colonthree@outlook.com>
2025-09-14 17:55:43 -0400

    Delete HelloWorldProject

--- HelloWorldProject/main.cpp
+++
@@ -1,9 +0,0 @@
-// This project is a basic example of a code::blocks project. There are many different project templates, this one being a Console Application.
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
